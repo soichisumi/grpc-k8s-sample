0e75f74f3e03c2d917191bf83de796d34fabd327
Soichi Sumi <soichi.sumi@gmail.com>
2019-02-20 14:36:54 +0900

    add interceptor to api

--- api/api.go
+++ api/api.go
@@ -7,60 +7,21 @@ import (
 	"github.com/davecgh/go-spew/spew"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/soichisumi/grpc-auth-sample/api-pb"
-	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/status"
-	"io/ioutil"
-	"log"
 	"time"
 )
 
 type UserServiceServer struct {
-	Users map[string]apipb.User // Todo: goroutine unsafe
-	PrivKey *rsa.PrivateKey // to generate token
-	PubKey *rsa.PublicKey // to validate token
+	Users   map[string]apipb.User // Todo: goroutine unsafe
+	PrivKey *rsa.PrivateKey
 }
 
-func validateToken(token string, publicKey *rsa.PublicKey) (*jwt.Token, error) {
-	jwtToken, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
-		if _, ok := t.Method.(*jwt.SigningMethodRSA); !ok {
-			log.Printf("Unexpected signing method: %v", t.Header["alg"])
-			return nil, fmt.Errorf("invalid token")
-		}
-		return publicKey, nil
-	})
-	if err == nil && jwtToken.Valid {
-		return jwtToken, nil
-	}
-	return nil, err
-}
-
-func NewServer(rsaPrivateKeyPath string, rsaPublicKeyPath string) (*UserServiceServer, error) {
-	privKey, err := ioutil.ReadFile(rsaPrivateKeyPath)
-	if err != nil {
-		return nil, fmt.Errorf("Error reading the jwt private key: %s", err)
-	}
-	parsedPrivKey, err := jwt.ParseRSAPrivateKeyFromPEM(privKey)
-	if err != nil {
-		return nil, fmt.Errorf("Error parsing the jwt private key: %s", err)
-	}
-
-	pubKey, err := ioutil.ReadFile(rsaPublicKeyPath)
-	if err != nil {
-		return nil, fmt.Errorf("Error reading the jwt public key: %s", err)
-	}
-	parsedPubKey, err := jwt.ParseRSAPublicKeyFromPEM(pubKey)
-	if err != nil {
-		return nil, fmt.Errorf("Error parsing the jwt public key: %s", err)
-	}
-
+func NewServer(rsaPrivateKey *rsa.PrivateKey) (*UserServiceServer, error) {
 	userDB := make(map[string]apipb.User)
-
 	return &UserServiceServer{
-		Users: userDB,
-		PrivKey: parsedPrivKey,
-		PubKey: parsedPubKey,
+		Users:   userDB,
+		PrivKey: rsaPrivateKey,
 	}, nil
 }
 
@@ -79,23 +40,8 @@ func (s *UserServiceServer) AddUser(ctx context.Context, req *apipb.AddUserReque
 	return &apipb.AddUserResponse{}, nil
 }
 
-
 // authorization required
 func (s *UserServiceServer) GetUser(ctx context.Context, req *apipb.GetUserRequest) (*apipb.GetUserResponse, error) {
-	md, ok := metadata.FromIncomingContext(ctx)
-	if !ok {
-		return nil, grpc.Errorf(codes.Unauthenticated, "valid token required. md: %+v", md)
-	}
-
-	jwtToken, ok := md["authorization"]
-	if !ok {
-		return nil, grpc.Errorf(codes.Unauthenticated, "valid token required. authorization not found. md: %+v", md)
-	}
-
-	_, err := validateToken(jwtToken[0], s.PubKey)
-	if err != nil {
-		return &apipb.GetUserResponse{}, status.Error(codes.Unauthenticated, fmt.Sprintf("valid token required. pubkey error: %+v", err))
-	}
 
 	username := req.Username
 	if username == "" {
@@ -138,6 +84,5 @@ func (s *UserServiceServer) Login(ctx context.Context, req *apipb.LoginRequest) 
 	if err != nil {
 		fmt.Println(err)
 	}
-	return  &apipb.LoginResponse{Token: tokenString}, nil
+	return &apipb.LoginResponse{Token: tokenString}, nil
 }
-
--- api/main.go
+++ api/main.go
@@ -1,28 +1,94 @@
 package main
 
 import (
+	"context"
+	"crypto/rsa"
 	"fmt"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/soichisumi/grpc-auth-sample/api-pb"
+	"github.com/soichisumi/grpc-auth-sample/auth"
+	"github.com/soichisumi/grpc-auth-sample/meta"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"io/ioutil"
 	"log"
 	"net"
 )
 
 const (
 	port = ":3000"
+	rsaPrivateKeyPath = "./privKey.pem"
+	rsaPublicKeyPath = "./privKey.pem.pub.pkcs8"
 )
+
+var (
+	PrivKey *rsa.PrivateKey       // to generate token
+	PubKey  *rsa.PublicKey        // to validate token
+)
+
+func validateToken(token string) (*jwt.Token, error) {
+	jwtToken, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodRSA); !ok {
+			log.Printf("Unexpected signing method: %v", t.Header["alg"])
+			return nil, fmt.Errorf("invalid token")
+		}
+		return PubKey, nil
+	})
+	if err == nil && jwtToken.Valid {
+		return jwtToken, nil
+	}
+	return nil, err
+}
+
+func authenticationFunc() auth.AuthenticationFunc {
+	return func(ctx context.Context) (context.Context, error) {
+		authorization, err := meta.Authorization(ctx)
+		if err != nil {
+			return nil, err
+		}
+
+		_, err = validateToken(authorization)
+		if err != nil {
+			return nil, err
+		}
+
+		return ctx, nil
+	}
+}
 
 func main() {
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %+v\n", err)
 	}
-	server, err := NewServer("./privKey.pem", "./privKey.pem.pub.pkcs8")
+
+	privKey, err := ioutil.ReadFile(rsaPrivateKeyPath)
+	if err != nil {
+		log.Fatalf("Error reading the jwt private key: %s", err)
+	}
+	parsedPrivKey, err := jwt.ParseRSAPrivateKeyFromPEM(privKey)
+	if err != nil {
+		log.Fatalf("Error parsing the jwt private key: %s", err)
+	}
+	PrivKey = parsedPrivKey
+
+	pubKey, err := ioutil.ReadFile(rsaPublicKeyPath)
+	if err != nil {
+		log.Fatalf("Error reading the jwt public key: %s", err)
+	}
+	parsedPubKey, err := jwt.ParseRSAPublicKeyFromPEM(pubKey)
+	if err != nil {
+		log.Fatalf("Error parsing the jwt public key: %s", err)
+	}
+	PubKey = parsedPubKey
+
+	server, err := NewServer(parsedPrivKey)
 	if err != nil{
 		log.Fatalf("failed to create server: %+v\n", err)
 	}
-	s := grpc.NewServer()
+	s := grpc.NewServer(
+		grpc.UnaryInterceptor(auth.AuthenticationInterceptor(authenticationFunc())),
+	)
 
 	apipb.RegisterUserServiceServer(s, server)
 	reflection.Register(s)
